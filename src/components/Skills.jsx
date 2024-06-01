568fac35deef45a13eb4f0104ebf71cc5e3d6404
Worlanyo Kwabla Kporfeame <kworlanyo@gmail.com>
2024-06-01 20:54:07 +0200

    added express and mongodb icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,6 +11,8 @@ import github from "../assets/icon-github.svg";
 import css from "../assets/icon-css.svg";
 import vscode from "../assets/icon-vscode.svg";
 import styled from "../assets/icon-styled-components.svg";
+import express from "../assets/icon-express-light.svg";
+import mongoDB from "../assets/icon-mongodb.svg";
 // import avatar from "../assets/avatar.png";
 // import aboutImg from "../assets/About-image.png";
 
@@ -26,6 +28,14 @@ const technologies = [
   {
     name: "Node.js",
     image: nodejs,
+  },
+  {
+    name: "Express",
+    image: express,
+  },
+  {
+    name: "MongoDB",
+    image: mongoDB,
   },
   {
     name: "Sass/Scss",
@@ -129,8 +139,7 @@ function Skills() {
       <div
         className="w-full py-16 bg-[#030712] md:py-20 2xl:py-24"
         style={{
-          background:
-            "radial-gradient(farthest-corner at 2000px 1000px, rgb(50, 66, 109) 0%, rgb(3, 7, 18) 67%)",
+          background: "radial-gradient(farthest-corner at 2000px 1000px, rgb(50, 66, 109) 0%, rgb(3, 7, 18) 67%)",
         }}
       >
         <div className="flex flex-col w-full gap-6 px-4 mx-auto max-w-7xl md:gap-12 md:px-8">
@@ -141,19 +150,13 @@ function Skills() {
               </div>
             </div>
             <p className="max-w-2xl text-lg text-center text-gray-200 text-normal md:text-xl">
-              The skills, tools and technologies I use to bring your products to
-              life
+              The skills, tools and technologies I use to bring your products to life
             </p>
           </div>
           <div className="grid grid-cols-3 text-gray-200 gap-y-4 md:grid-cols-6 md:gap-y-8 lg:grid-cols-8 lg:gap-y-12">
             {technologies.map((tech, i) => {
               return (
-                <div
-                  key={i}
-                  className="flex flex-col items-center gap-2"
-                  data-aos="zoom-in"
-                  data-aos-duration="1000"
-                >
+                <div key={i} className="flex flex-col items-center gap-2" data-aos="zoom-in" data-aos-duration="1000">
                   <img
                     src={tech.image}
                     alt={tech.name}
@@ -162,9 +165,7 @@ function Skills() {
                     // height={64}
                     className="transition-transform duration-300 md:hover:scale-110"
                   />
-                  <p className="text-base text-center text-normal md:text-lg">
-                    {tech.name}
-                  </p>
+                  <p className="text-base text-center text-normal md:text-lg">{tech.name}</p>
                 </div>
               );
             })}
